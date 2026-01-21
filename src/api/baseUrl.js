e36d59587ed66298f29eeaeaa322e753d9536346
Ajinormotor <teiteiwisdom@gmail.com>
2026-01-21 15:11:02 +0100

    fix: update urls

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const baseUrl = axios.create({
-  baseURL: import.meta.process.env._API_URL ?? 'http://localhost:5000/api/v1', 
+  baseURL: import.meta.process.env.VITE_API_URL ?? 'http://localhost:5000/api/v1', 
   withCredentials: true
 })
